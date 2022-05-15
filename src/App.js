9a5fa38357e3d29be7b894c1a7d84f44ffdaacda
TAATA EARL <fredvuni809@gmail.com>
2022-05-15 12:25:34 +0300

    Added loader to the app

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import React,{useEffect,useState} from "react"
 import "./App.css"
 import  Card from"./Card"
 import SearchIcon from "./search.svg"
+import Loader from './Loader'
 
 const API_URL = "https://www.omdbapi.com?apikey=b3ee82b4"
 
 const App = () =>{
     const [searchTitle,setSearchTitle] = useState("")
     const [movies,setMovies] = useState([])
+    const [loading,setLoading] = useState(false)
 
     const searchMovies = (e) =>{
         let searchValue = e.target.value
-        setSearchTitle(searchValue)
+        setSearchTitle(searchValue) 
     }
 
     const getMovies = async (title) =>{
         let response = await fetch(`${API_URL}&s=${title}`,{mode: 'cors'})
         let data = await response.json()
         setMovies(data.Search)
+        setLoading(true)
     }
 
     const movieSearch = () =>{
+        setLoading(false)
         getMovies(searchTitle)
     }
 
-    useEffect(()=>{
+    useEffect(()=>{ 
         getMovies('spy')   
     },[]);
 
@@ -41,6 +45,7 @@ const App = () =>{
             </div>
             <div className="container">
                 {
+                    loading ?
                     movies?.length > 0  ? 
                     movies.map((movie)=>(
                         <Card movie={movie} key={movie.imdbID}/> 
@@ -49,6 +54,8 @@ const App = () =>{
                     <div className="empty">
                         <h2>The movie does not exist</h2>
                     </div>
+                    :
+                    <Loader/>
                 }
             </div>
         </div>
